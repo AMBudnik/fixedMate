8d6e8ce5c306ad3933a9fb513dbb6ef879d5f36d
Aleksandra Budnik <qmpelka1@gmail.com>
2015-05-23 13:46:20 +0200

    Styling and fixes

--- script/main.js
+++ script/main.js
@@ -1,5 +1,15 @@
 (function() {
 
+    var mate = document.querySelectorAll('.fixed-mate-left, .fixed-mate-right');
+
+    for (var i = 0, len = mate.length; i < len; i++) {
+        var wrapper = document.createElement('div');
+
+        wrapper.innerHTML = mate[i].innerHTML;
+        mate[i].textContent = '';
+        mate[i].appendChild(wrapper);
+        wrapper.classList.add('fixed-mate-wrapper');
+    }
 
     document.addEventListener('click', function(event) {
         if (event.target.classList.contains('fixed-mate-left') || event.target.classList.contains('fixed-mate-right')) {
